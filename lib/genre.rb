6a507dba6b48557d2bfb848f6257a9a885462524
ryan m <contact@ryanmeek.com>
2019-07-05 22:56:32 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,35 +1,33 @@
-class Genre 
+class Genre
   extend Concerns::Findable
-  attr_accessor :name 
-  attr_reader :songs 
+  attr_accessor :name
+  attr_reader :songs
   @@all = []
 
   def initialize(name)
     @name= name
     @songs = []
   end
-  
+
   def self.all
     @@all
-  end 
-  
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
-  def save
-    @@all << self 
   end
-  
-  def self.create(thing)
-    self.new(thing).tap do |genre|
-    genre.save
-  end
-end
 
-def songs
-  @songs
-end 
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap { |genre| genre.save }
+  end
+
+  def songs
+    @songs
+  end 
 
 def artists
   @artists = []
