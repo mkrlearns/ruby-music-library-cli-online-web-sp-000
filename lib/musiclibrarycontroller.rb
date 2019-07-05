c1f0b9144a77eedb9bfa7ed4df2623b03d9ee7fa
ryan m <contact@ryanmeek.com>
2019-07-05 23:13:23 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,7 +17,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
 
-      user_input = gets 
+      input = gets.chomp
       case user_input  
         when 'list songs' 
           list_songs 
