4eab2c4fbeb2ccd6345beffa795b31628f9b58ce
ryan m <contact@ryanmeek.com>
2019-07-05 22:46:06 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+class Artist  
+  extend Concerns::Findable
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.create(thing)
+    self.new(thing).tap do |artist|
+    artist.save
+  end
+end
+ 
+ def songs 
+    @songs 
+  end
+  
+  def add_song(song)
+    if song.artist == self 
+    self.songs << song unless self.songs.include?(song) 
+  else 
+    song.artist = self 
+    self.songs << song unless self.songs.include?(song)
+  end
+end
+
+def genres
+  @genres = []
+  self.songs.collect do |song| 
+  @genres << song.genre unless @genres.include?(song.genre)
+end 
+@genres
+end
+
+
+end
