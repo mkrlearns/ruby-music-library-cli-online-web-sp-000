64f20496aa32e6de116737496fe53e776ba3b6f0
ryan m <contact@ryanmeek.com>
2019-07-05 23:46:03 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -44,17 +44,17 @@ class MusicLibraryController
     sort(Song, "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}")
   end
 
-  def list_artists
-    sort(Artist).each.with_index(1) do |artist, index|
-      puts "#{index}. #{artist.name}"
-    end
-  end
+  # def list_artists
+  #   sort(Artist).each.with_index(1) do |artist, index|
+  #     puts "#{index}. #{artist.name}"
+  #   end
+  # end
 
-  def list_genres
-    sort(Genre).each.with_index(1) do |genre, index|
-      puts "#{index}. #{genre.name}"
-    end
-  end
+  # def list_genres
+  #   sort(Genre).each.with_index(1) do |genre, index|
+  #     puts "#{index}. #{genre.name}"
+  #   end
+  # end
 
   # def list_songs_by_artist
   #   puts "Please, enter the name of an artist:"
@@ -69,17 +69,17 @@ class MusicLibraryController
   #   end
   # end
 
-  # def list_artists
-  #   sorted_artists = Artist.all.sort_by { |artist| artist.name }
-  #   sorted_artists.each.with_index(1) do |artist, index|
-  #     puts "#{index}. #{artist.name}"
-  #   end
-  # end
+  def list_artists
+    sorted_artists = Artist.all.sort_by { |artist| artist.name }
+    sorted_artists.each.with_index(1) do |artist, index|
+      puts "#{index}. #{artist.name}"
+    end
+  end
 
-  # def list_genres
-  #   sorted_genres = Genre.all.sort_by { |genre| genre.name }
-  #   sorted_genres.each.with_index(1) do |genre, index|
-  #     puts "#{index}. #{genre.name}"
-  #   end
-  # end
+  def list_genres
+    sorted_genres = Genre.all.sort_by { |genre| genre.name }
+    sorted_genres.each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
 end
