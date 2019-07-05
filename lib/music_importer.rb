782c88b1217f40f09c684529e9b01474a7da23ff
ryan m <contact@ryanmeek.com>
2019-07-05 23:04:52 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,17 +1,15 @@
 class MusicImporter
   attr_accessor :path
-  
+
   def initialize(path)
     @path = path
   end
 
   def files
-    Dir.glob("#{path}/*").map{|file| file.gsub("#{path}/", '')}
+    Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/", '') }
   end
-  
+
   def import
     self.files.each { |file| Song.create_from_filename(file) }
   end
-end
-  
 end
