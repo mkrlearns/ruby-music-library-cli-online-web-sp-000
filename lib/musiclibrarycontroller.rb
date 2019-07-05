3e49f5d0b56f09a1df06478ec983dbbdbf66a2e0
ryan m <contact@ryanmeek.com>
2019-07-05 23:09:28 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -3,3 +3,15 @@ class MusicLibraryController
   def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
   end
+
+  def call
+    input = ''
+  while input != 'exit'
+    input = gets.chomp
+    case input
+    when exit
+      puts "Bye"
+    end
+  end
+end
+end
