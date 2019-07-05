92f461359acf1c849a4f65d80b980f960d2acf7e
ryan m <contact@ryanmeek.com>
2019-07-05 23:34:41 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -35,6 +35,8 @@ class MusicLibraryController
     end 
   end
 
+  def sort(klass, item)
+    
   def list_songs
     sorted_songs = Song.all.sort_by { |song| song.name }
     sorted_songs.each.with_index(1) do |song, index|
