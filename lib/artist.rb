40ec519a62b2cc8c4aad08cd33e81f4557bd01df
ryan m <contact@ryanmeek.com>
2019-07-06 01:09:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist  
+  extend Concerns::Findable
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.create(name)
+    self.new(name).tap { |artist| artist.save }
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self if song.artist != self
+    @songs << song unless @songs.include?(song)
+  end
+
+  def genres
+    songs.map { |song| song.genre }.uniq
+  end 
+end
---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap { |song| song.save }
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(name)
+    artist, song, genre = name.split(" - ")
+    file = genre.gsub(".mp3", "")
+    genre = Genre.find_or_create_by_name(file)
+    artist = Artist.find_or_create_by_name(artist)
+    new(song, artist, genre)
+  end
+
+  def self.create_from_filename(name)
+    new_from_filename(name).save
+  end
+end
